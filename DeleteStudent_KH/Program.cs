bb089de42c5dba03ef8f5acc7670bd377a2b9bfe
ED <yinghua.lin@ischool.com.tw>
2017-11-13 14:51:30 +0800

    2017/11/13 穎驊調整 高雄項目 [06-01][03] 修改學生狀態沒有上傳局端 完畢

---
+++ DeleteStudent_KH/Program.cs
@@ -0,0 +1,18 @@
+﻿using FISCA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeleteStudent_KH
+{
+    public class Program
+    {
+        [MainMethod(StartupPriority.FirstAsynchronized)]
+        public static void Main()
+        {
+            // 註冊
+            FISCA.InteractionService.RegisterAPI<IRewriteAPI_JH.IStudentDeleteStudentAPI>(new StudentDeleteStudentItem());         
+        }
+    }
+}
